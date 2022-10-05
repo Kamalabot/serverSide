ae26cd3a95af34492227a5d70e31a5fd3c149e2a
Kamalraj M M <kamaljp@gmail.com>
2022-10-05 13:38:20 +0530

    Basic Node intro completed, pushed to glitch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const fs = require('fs')
 const app = express()
 
 const server = app.listen(3000,()=>{
@@ -13,3 +13,37 @@ app.use(express.static('webface'))//webface is the folder that has the static pa
 app.get('/interface',(request,response)=>{
     response.send('<html><h1>This data has come directly from server as text</h1>. <p>Through this programming interface, server can write to the client display.</p><p>This server is rendered from a service called glitch where the node.js modules are supporting this website.</p><p>The website is built on a laptop, and pushed to github, which then updates the page here</p></html>')    
 })
+
+app.get('/location/:lat/:long',getLocation)
+
+//when the url is triggered the
+function getLocation(request, response){
+    var data = request.params;
+    var reply = '';
+    if(data){
+        reply += `latitude :${data.lat}, longitude:${data.long}`;
+        reply += `You are ${Math.random()*10} mins from here.`
+    }
+    response.send(reply)
+}
+
+app.get('/details',getName)
+
+//when the url is triggered with the format http://127.0.0.1:3000/details?named=chec
+function getName(request, response){
+    var url = request.query; //select the 
+    if(url.name){
+        var reply = `Your detail is ${JSON.stringify(url)}`
+    } else {
+        var reply = `Your detail is missing.`
+    }
+    response.send(reply)
+}
+
+app.get('/all', getPenncolors)
+
+async function getPenncolors(request, response){
+    const pennColorData = fs.readFileSync('pennTagColored.json')
+    const pennColor = JSON.parse(pennColorData)
+    response.send(pennColor)
+}
