1ccefe7ebab9ba8586100f5cad1aaa69a2a46e33
Kamalraj M M <kamaljp@gmail.com>
2022-10-05 09:43:15 +0530

    added gitShortCut

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ const server = app.listen(3000,()=>{
 app.use(express.static('webface'))//webface is the folder that has the static pages
 
 app.get('/interface',(request,response)=>{
-    response.send('<html><h1>This data has come directly from server as text</h1>. <p>Through this programming interface, server can write to the client display</p></html>')    
+    response.send('<html><h1>This data has come directly from server as text</h1>. <p>Through this programming interface, server can write to the client display.</p><p>This server is rendered from a service called glitch where the node.js modules are supporting this website.</p><p>The website is built on a laptop, and pushed to github, which then updates the page here</p></html>')    
 })
